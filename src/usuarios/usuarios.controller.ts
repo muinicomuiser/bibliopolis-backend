e9e8588b0a9ae055ba60590aee5d861ae79475ef
muinicomuiser <nicodoneg@gmail.com>
2024-07-25 19:44:57 -0400

    Módulo de usuarios

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Post, Param, Delete, Body, Res } from '@nestjs/common';
+import { Usuario } from 'src/models/usuario';
+import { UsuarioDTO } from 'src/models/usuarioDTO';
+import { UsuariosService } from './usuarios.service';
+import { Response } from 'express';
+//// a. Registrar un nuevo usuario (Verificar si existe el usuario según el correo ingresado)
+//// b. Obtener un usuario según su id, en caso de que el usuario no exista devolver el código 404.
+//// c. Obtener todos los usuarios (excluir la password en la lista )
+//// d. Eliminar un usuario según su id
 @Controller('usuarios')
-export class UsuariosController {}
+export class UsuariosController {
+    constructor(private readonly servicioUsuarios: UsuariosService){}
+    /**Registrar un usuario nuevo.*/
+    @Post()
+    registrarUsuario(@Body() usuario: Usuario): void{
+        this.servicioUsuarios.registrarUsuario(usuario);
+    }
+    
+    /**Obtener usuario según su id.     
+     * Asumiendo que debe ir sin la contraseña.     
+     * De no encontrar coincidencias, responder con 404.
+    */
+    @Get(':id')
+    obtenerUsuarioPorId(@Param('id') id: number, @Res() response: Response): void{
+        let usuarioPorId = this.servicioUsuarios.obtenerUsuarioPorId(id);
+        if(usuarioPorId){
+            response.status(200).send(usuarioPorId);
+        }
+        else{
+            response.status(404).send({mensaje: "Usuario no encontrado."});
+        }
+    }
+
+    /**Obtener todos los usuarios.*/
+    @Get()
+    obtenerUsuarios(): UsuarioDTO[]{
+        return this.servicioUsuarios.obtenerUsuarios();
+    }
+
+    /**Eliminar usuario según su id. */
+    @Delete(':id')
+    eliminarUsuarioPorId(@Param('id') id: number): void{
+        this.servicioUsuarios.eliminarUsuarioPorId(id);
+    }
+
+}
